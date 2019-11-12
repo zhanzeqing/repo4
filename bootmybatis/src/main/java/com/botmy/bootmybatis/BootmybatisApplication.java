5244b72419e87c36ef6505a953042a97da57f05a
root <root>
2019-11-12 11:58:49 +0800

    1111111

---
+++ bootmybatis/src/main/java/com/botmy/bootmybatis/BootmybatisApplication.java
@@ -0,0 +1,13 @@
+package com.botmy.bootmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootmybatisApplication.class, args);
+	}
+
+}
