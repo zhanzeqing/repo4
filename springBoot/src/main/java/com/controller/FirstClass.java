ea7b606b3b27cc334f219ff0b37ee04122242f81
root <root>
2019-11-12 11:56:49 +0800

    这是第一次commit

---
+++ springBoot/src/main/java/com/controller/FirstClass.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("你好git project");
+        System.out.println("你好git project");
+        System.out.println("你好git project");
+        System.out.println("你好git project");
+        System.out.println("你好git project");
+
+    }
+    public void test(){
+
+    }
+    public void test1(){
+
+    }
+    public void test2(){
+
+    }
+
+}
