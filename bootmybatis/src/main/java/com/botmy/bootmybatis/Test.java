bdf38421edf268b488d2295bde3a44e092f3e0d1
root <root>
2019-11-12 12:30:46 +0800

    4444444444444

--- bootmybatis/src/main/java/com/botmy/bootmybatis/Test.java
+++ bootmybatis/src/main/java/com/botmy/bootmybatis/Test.java
@@ -3,6 +3,9 @@ package com.botmy.bootmybatis;
 public class Test {
     public static void main(String[] args) {
         System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
     }
     public void test(){}
     public void teddd(){
